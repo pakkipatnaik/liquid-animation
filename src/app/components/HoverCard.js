9c57c7ba8803c5fdad5581ba7ec9d925a0bc7cab
Pakki Padmavathi <pakkipatnaik1@gmail.com>
2024-08-06 18:00:07 +0530

    first commit

---
+++ src/app/components/HoverCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../page.module.css';
+import Image from 'next/image';
+
+export default function HoverCard({ imageSrc }) {
+  return (
+    <div>
+      <div className={styles.content}>
+        <Image 
+        src={imageSrc}
+        alt="Image" 
+        width={500} 
+        height={300} 
+        className={styles.card}
+      />
+      </div>
+    </div>
+  );
+}
